f1beff6d723697d4e9c8aca125ed767976ccec43
ruissouw <rohit16@live.com>
2025-06-03 00:21:31 +0800

    add home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import './index.css'
-import Navbar from "./components/Navbar"
+import Home from "./pages/Home"
 
 
 const App = () => {
   return (
-    <Navbar />
+    <Home />
   );
 };
 
