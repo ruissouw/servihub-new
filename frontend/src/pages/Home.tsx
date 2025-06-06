844bc4a9ef464d721ac2c29d02f81ee8d8f2c889
ruissouw <rohit16@live.com>
2025-06-06 22:02:09 +0800

    render view based on persona

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import Navbar from "../components/Navbar"
 import Schedule from "../components/Schedule"
+import { useUserStore } from '@/stores/userStore'
+import BizSchedule from "../components/BizSchedule"
 
 const Home = () => {
+  const role = useUserStore(state => state.role)
   return (
     <>
       <div className='navbar'>
         <Navbar />
       </div>
       <div>
-        <Schedule />
+        {role === "customer" ? <Schedule /> : <BizSchedule />}
       </div>
     </>
   )
