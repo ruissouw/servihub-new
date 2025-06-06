4a9dbc16345047760143518964c4e32c7ebeb0c6
ruissouw <rohit16@live.com>
2025-06-06 12:52:10 +0800

    add functions to alter status and prevent duplication

--- frontend/src/stores/useBookingStore.tsx
+++ frontend/src/stores/useBookingStore.tsx
@@ -4,14 +4,28 @@ import type { BookingEvent } from '@/types'
 type BookingStore = {
   bookings: BookingEvent[]
   addBookings: (events: BookingEvent[]) => void
+  updateBookingStatus: (ids: string[], newStatus: BookingEvent['status']) => void
+  seeded: boolean
+  markSeeded: () => void
 }
 
-export const useBookingStore = create<BookingStore>((set) => ({
+export const useBookingStore = create<BookingStore>((set, get) => ({
   bookings: [],
-  addBookings: (events) =>
-    set((state) => ({
-      bookings: [...state.bookings, ...events],
+  seeded: false,
+  addBookings: events => {
+    const existing = get().bookings
+    const newEvents = events.filter(
+      e => !existing.find((existingEvent) => existingEvent.id === e.id)
+    )
+    set({
+      bookings: [...existing, ...newEvents],
+    })
+  },
+  updateBookingStatus: (ids, newStatus) =>
+    set(state => ({
+      bookings: state.bookings.map((event) =>
+        ids.includes(event.id) ? { ...event, status: newStatus } : event
+      ),
     })),
+  markSeeded: () => set({ seeded: true }),
 }))
-
-
