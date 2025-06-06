b95839e2e2b228e88f519efdcaa75e25a2c47e23
ruissouw <rohit16@live.com>
2025-06-06 17:05:48 +0800

    add edit logo

--- frontend/src/components/EventView.tsx
+++ frontend/src/components/EventView.tsx
@@ -1,20 +1,32 @@
-import { X } from 'lucide-react' 
+import { X, Pencil } from 'lucide-react' 
 import type { BookingEvent } from '@/types'
+import { useNavigate } from 'react-router-dom'
 
 const EventView = ({ event, title, onMarkDelete }: { event: BookingEvent, title: string, onMarkDelete: (e: BookingEvent) => void }) => {
+  const navigate = useNavigate();
+  
   return (
-    <div className="flex items-center justify-between gap-1 px-1">
+    <div className="flex items-center justify-between px-1">
       <span>{title}</span>
-      <button
-        onClick={(e) => {
-          e.stopPropagation()
-          onMarkDelete(event)
-        }}
-        className="text-red-500 hover:text-red-700"
-        title="Mark for deletion"
-      >
-        <X size={15} />
-      </button>
+      <div className="flex items-center gap-1">
+        <button
+          onClick={() => navigate(`/schedule/${event.id}`)}
+          className="text-red-500 hover:text-red-700"
+          title="Edit Booking"
+        >
+          <Pencil size={15} />
+        </button>
+        <button
+          onClick={(e) => {
+            e.stopPropagation()
+            onMarkDelete(event)
+          }}
+          className="text-red-500 hover:text-red-700"
+          title="Mark for deletion"
+        >
+          <X size={15} />
+        </button>
+      </div>
     </div>
   )
 }
