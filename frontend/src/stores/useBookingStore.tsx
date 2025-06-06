7ab7d769879d74003a19a67f8d8a61fdbc655ba8
ruissouw <rohit16@live.com>
2025-06-06 17:10:54 +0800

    support updating events and search by id

--- frontend/src/stores/useBookingStore.tsx
+++ frontend/src/stores/useBookingStore.tsx
@@ -7,25 +7,38 @@ type BookingStore = {
   updateBookingStatus: (ids: string[], newStatus: BookingEvent['status']) => void
   seeded: boolean
   markSeeded: () => void
+  updateBooking: (updatedEvent: BookingEvent) => void
+  getBookingById: (id: string) => BookingEvent | undefined
 }
 
-export const useBookingStore = create<BookingStore>((set, get) => ({
-  bookings: [],
-  seeded: false,
-  addBookings: events => {
-    const existing = get().bookings
-    const newEvents = events.filter(
-      e => !existing.find((existingEvent) => existingEvent.id === e.id)
-    )
-    set({
-      bookings: [...existing, ...newEvents],
-    })
-  },
-  updateBookingStatus: (ids, newStatus) =>
-    set(state => ({
-      bookings: state.bookings.map((event) =>
-        ids.includes(event.id) ? { ...event, status: newStatus } : event
-      ),
-    })),
-  markSeeded: () => set({ seeded: true }),
-}))
+export const useBookingStore = create<BookingStore>(
+  (set, get) => ({
+    bookings: [],
+    seeded: false,
+    addBookings: events => {
+      const existing = get().bookings
+      const newEvents = events.filter(
+        e => !existing.find((existingEvent) => existingEvent.id === e.id)
+      )
+      set({
+        bookings: [...existing, ...newEvents],
+      })
+    },
+    updateBookingStatus: (ids, newStatus) =>
+      set(state => ({
+        bookings: state.bookings.map((event) =>
+          ids.includes(event.id) ? { ...event, status: newStatus } : event
+        ),
+      })),
+    markSeeded: () => set({ seeded: true }),
+    updateBooking: (updated) =>
+      set((state) => ({
+        bookings: state.bookings.map((b) =>
+          b.id === updated.id ? updated : b
+        ),
+      })),
+    getBookingById: (id) => {
+      return get().bookings.find((event) => event.id === id);
+    },
+  })
+)
